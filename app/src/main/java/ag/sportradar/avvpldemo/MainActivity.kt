6f245353de7382db62b784f224ef722d76155401
c.frank <c.frank@sportradar.com>
2020-11-23 16:40:32 +0100

    Instantiates player and calls setup with config url.

--- app/src/main/java/ag/sportradar/avvpldemo/MainActivity.kt
+++ app/src/main/java/ag/sportradar/avvpldemo/MainActivity.kt
@@ -1,5 +1,8 @@
 package ag.sportradar.avvpldemo
 
+import ag.sportradar.avvplayer.config.AVVConfig
+import ag.sportradar.avvplayer.config.AVVConfigUrl
+import ag.sportradar.avvplayer.player.AVVPlayerBuilder
 import ag.sportradar.avvplayer.player.licencing.AVVLicenceCheckListener
 import ag.sportradar.avvplayer.player.settings.AVVSettings
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         /*README.md Checking for a valid licence */
-        AVVSettings.instance.initLicence(this,"your_licence_key",
+        AVVSettings.instance.initLicence(this, "your_licence_key",
             object : AVVLicenceCheckListener {
                 override fun onLicenceValidated(valid: Boolean) {}
             });
+
+        /*README.md Instantiate the Player */
+        val player = AVVPlayerBuilder(this)
+            .build()
+
+        /*README.md Start the video passing a Video Configuration to the player */
+        player.setup(AVVConfigUrl("https://www.badmintoneurope.tv/api/v2/content/92179/player-setting"))
     }
 }
