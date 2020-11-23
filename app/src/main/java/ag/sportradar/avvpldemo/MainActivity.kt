2256194698b9482bca85d9db66b518b2e27fb20d
c.frank <c.frank@sportradar.com>
2020-11-23 18:38:28 +0100

    Implements config adaptation.

--- app/src/main/java/ag/sportradar/avvpldemo/MainActivity.kt
+++ app/src/main/java/ag/sportradar/avvpldemo/MainActivity.kt
@@ -1,11 +1,13 @@
 package ag.sportradar.avvpldemo
 
 import ag.sportradar.avvplayer.config.AVVConfig
+import ag.sportradar.avvplayer.config.AVVConfigAdaptationCallback
 import ag.sportradar.avvplayer.config.AVVConfigUrl
 import ag.sportradar.avvplayer.player.AVVPlayer
 import ag.sportradar.avvplayer.player.AVVPlayerBuilder
 import ag.sportradar.avvplayer.player.licencing.AVVLicenceCheckListener
 import ag.sportradar.avvplayer.player.settings.AVVSettings
+import ag.sportradar.avvplayer.task.http.AVVPostRequestData
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -30,7 +32,22 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         /*README.md Start the video passing a Video Configuration to the player */
-        player.setup(AVVConfigUrl("https://www.badmintoneurope.tv/api/v2/content/92179/player-setting"))
+        player.setup(AVVConfigUrl("https://www.badmintoneurope.tv/api/v2/content/92179/player-setting"), getConfigAdaptation())
+    }
+
+
+    private fun getConfigAdaptation(): AVVConfigAdaptationCallback {
+        /*Changing the player configuration */
+        return object : AVVConfigAdaptationCallback() {
+
+            override fun adaptConfig(config: AVVConfig) {
+                /*Adding request headers to streamaccess*/
+                config.streamUrlProviderInfo.requestData = AVVPostRequestData(mapOf(Pair("authorization", "your auth token")))
+
+                /*Changing autoplay behavior*/
+                config.streamMetaData.autoPlay = true
+            }
+        }
     }
 
     //region README.md Handle lifecycle events
